9162768527c5f45a72181cdc7e0cee62db8e3ad6
divisionbyzero <93157782+divisionbyzero8@users.noreply.github.com>
2023-07-21 13:26:55 +0330

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x84fb717a11b926b50defcbf0aebbd391aca1002e";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
